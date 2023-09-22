8f6b8b716a1dafaca6854b41fd3fb630dd1f9cfe
Melissa <melissamichaud0319@gmail.com>
2023-09-22 02:41:24 -0400

    adding client folder

---
+++ backend/index.js
@@ -0,0 +1,89 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to MySQL Database");
+});
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) {
+      console.error("Error in POST /books:", err);
+      return res
+        .status(500)
+        .json({ message: "Internal Server Error", error: err });
+      return res.json(data);
+    }
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id = ?";
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.status(500).json({ message: "Internal Server Error" });
+    return res.json(data);
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q =
+    "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+  db.query(q, [...values, bookId], (err, data) => {
+    if (err) {
+      console.error("Error in POST /books:", err);
+      return res
+        .status(500)
+        .json({ message: "Internal Server Error", error: err });
+    }
+    return res.json(data);
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend.");
+});
